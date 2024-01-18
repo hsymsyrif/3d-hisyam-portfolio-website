dade5bd26bb59088aca795d91b5d55e67b7256e8
hsymsyrif <hisyams191@gmail.com>
2024-01-18 14:39:39 +0700

    updated final

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -47,7 +47,6 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <br />
         <p className={`${styles.sectionSubText} text-center`}>What I have done so far</p>
         <h2 className={`${styles.sectionHeadText} text-center`}>Work Experience.</h2>
       </motion.div>
